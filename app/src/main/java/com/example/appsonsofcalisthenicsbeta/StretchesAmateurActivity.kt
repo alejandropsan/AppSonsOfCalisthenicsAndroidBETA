d958f3b4a2cfa17803186b60e8a433f0a96eca3c
alejandropsan <52634393+alejandropsan@users.noreply.github.com>
2022-11-09 18:54:17 +0100

    second commit

--- app/src/main/java/com/example/appsonsofcalisthenicsbeta/StretchesAmateurActivity.kt
+++ app/src/main/java/com/example/appsonsofcalisthenicsbeta/StretchesAmateurActivity.kt
@@ -1,12 +1,33 @@
 package com.example.appsonsofcalisthenicsbeta
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class StretchesAmateurActivity : AppCompatActivity() {
+
+    private lateinit var vvWeb: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stretches_amateur)
 
+        var vvWeb = findViewById<VideoView>(R.id.vvStretches)
+        var mcWeb = MediaController(this)
+        var videoDirecc = Uri.parse("android.resource://estiramientos_piernas.mp4" + R.raw.estiramientos_piernas)
+
+            vvWeb.setVideoPath(videoDirecc.toString())
+
+            mcWeb.setAnchorView(vvWeb)
+            vvWeb.setMediaController(mcWeb)
+
+
+
+
     }
 }
+
